550e5f7bf335627413ffc6ba6d1ee56666222ab2
HELLATRIGGERR <mpokarec@gmail.com>
2025-02-15 20:49:35 +0200

    Змінив спосів малювання змії, каже так краще(пропало дьоргання)

--- snake.py
+++ snake.py
@@ -1,8 +1,7 @@
 import pygame
 
 def draw_snake(game_window, snake_body):
-    for pos in snake_body:
-        myimage = pygame.image.load(pos[-1]).convert()
-        game_window.blit(myimage, pos[0])
-        pygame.display.flip()
-        pygame.display.update()
+
+    for segment in snake_body:
+        image = pygame.image.load(segment[-1]).convert_alpha()
+        game_window.blit(image, segment[0])
