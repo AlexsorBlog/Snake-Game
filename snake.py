6805df8d00c2a4afa3228c33e1951810c513ca44
HELLATRIGGERR <mpokarec@gmail.com>
2025-02-16 17:23:30 +0200

    Додав хвіст, та прибрав лаги

--- snake.py
+++ snake.py
@@ -1,7 +1,6 @@
 import pygame
 
 def draw_snake(game_window, snake_body):
-
     for segment in snake_body:
         image = pygame.image.load(segment[-1]).convert_alpha()
         game_window.blit(image, segment[0])
